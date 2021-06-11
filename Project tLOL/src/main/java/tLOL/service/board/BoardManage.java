6cfbd2ae3bd1f2bffa68ef6c1850300a2a2e0de6
LuaJ <catapower@naver.com>
2021-06-11 10:37:38 +0900

    Revert "Revert "Merge remote-tracking branch 'origin/manageTest-SH' into main-v0.6""
    This reverts commit 3876cdfa6ba6a65857abb1e1e8ff36649dcf6653.

---
+++ Project tLOL/src/main/java/tLOL/service/board/BoardManage.java
@@ -0,0 +1,50 @@
+package tLOL.service.board;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import tLOL.dao.ArticleDao;
+import tLOL.dao.BoardDao;
+import tLOL.model.Article;
+import tLOL.service.CommandProcess;
+
+public class BoardManage implements CommandProcess {
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) {
+		final int ROW_PER_PAGE = 10;
+		final int PAGE_PER_BLOCK = 10;
+		String pageNum = request.getParameter("pageNum");
+		if (pageNum == null || pageNum.equals(""))
+			pageNum = "1";
+		int currentPage = Integer.parseInt(pageNum);		
+		ArticleDao ad = ArticleDao.getInstance();
+		HttpSession session = request.getSession();
+	    if (session == null || session.getAttribute("member_num") == null ) {
+	    	return "../sessionChk";
+	    } 
+	    System.out.println("1");
+		int total = ad.getManageTotal();	
+		System.out.println("2");
+		int startRow = (currentPage - 1) * ROW_PER_PAGE + 1;
+		int endRow = startRow + ROW_PER_PAGE - 1;
+		int number = total - startRow + 1;
+		int totalPage = (int) Math.ceil((double) total / ROW_PER_PAGE);
+		int startPage = currentPage - (currentPage - 1) % PAGE_PER_BLOCK;
+		int endPage = startPage + PAGE_PER_BLOCK - 1;
+		if (endPage > totalPage)
+			endPage = totalPage;		
+		List<Article> list = ad.manageList(startRow, endRow);	
+		System.out.println("3");
+		request.setAttribute("list", list);
+		request.setAttribute("PAGE_PER_BLOCK", PAGE_PER_BLOCK);
+		request.setAttribute("number", number);
+		request.setAttribute("currentPage", currentPage);
+		request.setAttribute("startPage", startPage);
+		request.setAttribute("endPage", endPage);
+		request.setAttribute("totalPage", totalPage);		
+		return "manageArticle";				
+	}
+}
